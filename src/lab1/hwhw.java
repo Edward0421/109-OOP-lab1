f23d27f4c4a5a73c951de360072cfb681f0547c0
user <user@EA09>
2020-09-29 09:34:58 +0800

    adad

--- src/lab1/hwhw.java
+++ src/lab1/hwhw.java
@@ -2,6 +2,8 @@ package lab1;
 
 public class hwhw {
 	public static void main(string[] args) {
-		System.out.println("Hello oop");
+		System.out.println("I'm ªL­õÞ³");
+		System.out.println("My Nid is D0843601");
+		System.out.println("This is lab 1 hw");
 	}
 }
