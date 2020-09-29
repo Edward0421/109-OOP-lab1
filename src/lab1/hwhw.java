e3d611d73b4656c6bd572687d138adb367335dc8
user <user@EA09>
2020-09-29 09:39:56 +0800

    chinese to eng

--- src/lab1/hwhw.java
+++ src/lab1/hwhw.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class hwhw {
 	public static void main(string[] args) {
-		System.out.println("I'm ªL­õÞ³");
+		System.out.println("I'm Edward");
 		System.out.println("My Nid is D0843601");
 		System.out.println("This is lab 1 hw");
 	}
